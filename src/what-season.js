d5a8491dbaade2955c4171992e22e4163947a164
plur <plur@live.ru>
2022-09-07 13:30:00 +0300

    add: what-season (all test except tricky ones)

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSeason(date) {
+
+  console.log(date)
+
+  const isValidDate = (d) => {
+    return d instanceof Date && !isNaN(d.valueOf()); //TODO: dont understand the tricky shit 
+  }
+
+  let season = '';
+  if (date === undefined) return "Unable to determine the time of year!";
+  if (!isValidDate(date)) throw new Error("Invalid date!");
+  const springStart = new Date(date.getFullYear(), 2, 1);
+  const summerStart = new Date(date.getFullYear(), 5, 1);
+  const autumnStart = new Date(date.getFullYear(), 8, 1);
+  const winterStart = new Date(date.getFullYear(), 11, 1);
+
+  if (date >= springStart && date < summerStart) {
+    season = 'spring';
+  } else if (date >= summerStart && date < autumnStart) {
+    season = 'summer';
+  } else if (date >= autumnStart && date < winterStart) {
+    season = 'autumn';
+  } else if (date >= winterStart && date < springStart) {
+    season = 'winter';
+  } else {
+    return 'winter';
+  }
+  return season;
 }
 
 module.exports = {
