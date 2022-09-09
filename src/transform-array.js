d61343e50fb3575f6563e1217db23d61985557c9
undermouse <plur@live.ru>
2022-09-09 08:25:21 +0300

    add: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (arr instanceof Array === false) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let resArr = [];
+  let tempArr = [];
+  let controlSeq = "";
+
+  let doubleNext = "--double-next";
+  let doublePrev = "--double-prev";
+  let disNext = "--discard-next";
+  let disPrev = "--discard-prev";
+
+  tempArr = [...arr];
+
+  arr.forEach((el) => {
+    let next = Number(arr.indexOf(el) + 1);
+    let prev = Number(arr.indexOf(el) - 1);
+    let curr = Number(arr.indexOf(el));
+
+    if (typeof el === "string") {
+      controlSeq = el;
+      if (controlSeq === doubleNext) {
+        if (tempArr[next]) {
+         tempArr[curr] = tempArr[next]; 
+        } else {
+          tempArr[curr] = 'del';
+        }
+      } else if (controlSeq === doublePrev) {
+          if (tempArr[prev]) {
+           tempArr[curr] = tempArr[next]; 
+          } else {
+            tempArr[prev] = 'del';
+          }
+        tempArr[curr] = tempArr[prev];
+
+      } else if (controlSeq === disNext && tempArr[next] !== -1) {
+        tempArr[curr] = "del";
+        tempArr[next] = "del";
+      } else if (controlSeq === disPrev && tempArr[prev] !== -1) {
+        tempArr[prev] = "del";
+        tempArr[curr] = "del";
+      }
+    }
+  });
+  for (let i = 0; i < tempArr.length; i++) {
+    if (tempArr[i] !== "del") {
+      resArr.push(tempArr[i]);
+    }
+  }
+  return resArr;
 }
 
 module.exports = {
