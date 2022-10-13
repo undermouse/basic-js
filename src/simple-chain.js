b59cb24e200e4735966b027bf0cd84c6a646bbf7
plur <plur@live.ru>
2022-10-13 10:43:47 +0300

    feat: chainMaker done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,62 +4,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
- const errMsg = "You can't remove incorrect link!";
- function isInt(value) {
-   return (
-     !isNaN(value) &&
-     parseInt(Number(value)) == value &&
-     !isNaN(parseInt(value, 10))
-   );
- }
- String.prototype.addLink = function () {
-   return this + "+";
- };
- 
- let resArr = [];
- let tmpArr = [];
- let resString = "";
- 
- const chainMaker = {
-   getLength() {
-     return resArr.length;
-   },
- 
-   addLink(value) {
-     if (!value) {
-       resArr.push(` ( ) `);
-       return this;
-     } else {
-       resArr.push(` (${value}) `);
-       return this;
-     }
-   },
-   removeLink(position) {
-     if (!isInt(position) || isNaN(position) || !position) {
-       throw new Error(errMsg);
-     }
-     if (position <0 || position > chainMaker.getLength()) {
+const errMsg = "You can't remove incorrect link!";
+//  function isInt(value) {
+//    return (
+//      !isNaN(value) &&
+//      parseInt(Number(value)) == value &&
+//      !isNaN(parseInt(value, 10))
+//    );
+//  }
+
+const chainMaker = {
+
+  resArr: [],
+
+  getLength() {
+    return this.resArr.length;
+  },
+
+  addLink(value = '') {
+    this.resArr.push(value);
+    return this;
+  },
+
+
+  removeLink(position) {
+    if (typeof position !== 'number' ||
+      !Number.isInteger(position) ||
+      position < 1 ||
+      position > this.resArr.length
+    ) {
+      this.resArr.length = 0;
       throw new Error(errMsg);
-     }
-     tmpArr = [...resArr];
-     tmpArr[position] = "del";
-     resArr = [];
-     tmpArr.forEach((elDel) => {
-       if (elDel !== "del") {
-         resArr.push(elDel);
-       }
-     });
-   },
- 
-   reverseChain() {
-     resArr.reverse();
-   },
- 
-   finishChain() {
-     resString = resArr.join("~~");
-     return resString;
-   }
- };
+    } else {
+      this.resArr.splice(position - 1, 1)
+    }
+    return this;
+  },
+
+  reverseChain() {
+    //resArr.reverse();
+    this.resArr = this.resArr.reverse();
+    return this;
+  },
+
+  finishChain() {
+    //  resString = resArr.join("~~");
+    //  return resString;
+
+    const result = this.resArr.map(ch => `( ${ch} )`)
+      .join('~~');
+    this.resArr.length = 0;
+    return result;
+  }
+};
 
 module.exports = {
   chainMaker
