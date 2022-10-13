a0037ba18ab950e1bb082489bfa6489636efb4f8
plur <plur@live.ru>
2022-10-13 11:31:55 +0300

    feat: mac / sum digits

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let group = n.match(/[0-9A-F]{2}/g);
+  return group.length === 6 && n === group.join('-');
 }
 module.exports = {
   isMAC48Address
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  if (n <= 9) {
+    return n
+  } else {
+    return getSumOfDigits(n.toString().split('').reduce((acc, current) => acc + +current, 0));
+  }
 }
 
 module.exports = {
